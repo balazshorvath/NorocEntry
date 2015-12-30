de8d846faf5257dc5c130f29e51709d398dfada4
balazshorvath <balazs.peter.horvath@gmail.com>
2015-12-30 19:11:02 +0100

    Network functions

--- Project2/Network.h
+++ Project2/Network.h
@@ -3,6 +3,6 @@
 SOCKET TCPServerInit(string port);
 SOCKET TCPAccept(SOCKET serverSocket);
 
-int TCPRecv(SOCKET client, char* buffer);
-int TCPSend(SOCKET client, char* buffer);
+int TCPRecv(SOCKET client, char* buffer, int size);
+int inline TCPSend(SOCKET client, char* buffer, int size);
 
